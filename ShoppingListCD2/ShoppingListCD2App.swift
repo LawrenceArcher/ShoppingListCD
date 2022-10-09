673d38ab227688b7f69b8bb9e6fe91da4bd246b0
LawrenceArcher <archer@hey.com>
2022-10-09 21:02:09 +0100

    Initial Commit

---
+++ ShoppingListCD2/ShoppingListCD2App.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListCD2App.swift
+//  ShoppingListCD2
+//
+//  Created by Lawrence Archer on 09/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListCD2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
