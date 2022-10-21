97fdfeb32e6bd3075b5d59894c848d9a49964375
LawrenceArcher <archer@hey.com>
2022-10-21 09:09:49 +0100

    add ability to select label on adding item. It looks like shit but it's OK

--- ShoppingListCD2/Views/AddItemView.swift
+++ ShoppingListCD2/Views/AddItemView.swift
@@ -12,18 +12,36 @@ struct AddItemView: View {
     @Environment(\.managedObjectContext) var moc
     @Environment(\.dismiss) var dismiss
     
+    let itemLabels: FetchRequest<ItemLabel>
+    
     @State private var name = ""
     @State private var quantity = 1
     @State private var unit = ""
+    @State private var itemLabel: ItemLabel?
+    
+    init() {
+        itemLabels = FetchRequest<ItemLabel>(entity: ItemLabel.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \ItemLabel.name, ascending: true)])
+    }
     
     var body: some View {
         NavigationView {
             Form {
-                Section {
+                Section (header: Text("Add item details")){
                     TextField("Item name", text: $name)
                     TextField("Quantity", value: $quantity, format: .number)
                         .keyboardType(.numberPad)
                     TextField("Unit", text: $unit)
+                }
+                Section (header: Text("Add Item Label")) {
+                    ForEach(itemLabels.wrappedValue) { item in
+                        HStack {
+                            Text(item.labelName)
+                                .foregroundColor(Color(item.labelColour))
+                                .onTapGesture {
+                                    itemLabel = item
+                                }
+                        }
+                    }
                 }
                 Section {
                     Button("Save") {
